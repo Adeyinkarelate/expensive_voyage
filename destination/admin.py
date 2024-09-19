3579b628012617f01e600c69254e562b3b8845b1
MMB <mailadeyinka2020@gmail.com>
2024-09-19 13:54:26 +0100

    second update

--- destination/admin.py
+++ destination/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Destination , PriceAdvantage
 # Register your models here.
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(PriceAdvantage)
